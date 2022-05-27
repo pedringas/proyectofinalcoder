f434d876189302e902c4b9075d74a82ed60722e5
Pconti10 <pconti10@gmail.com>
2022-05-27 19:28:21 -0300

    empezando pagina post

--- JS/js.js
+++ JS/js.js
@@ -1,21 +1,19 @@
-$(document).ready(function(){
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
-    $('#itemslider').carousel({ interval: 3000 });
-    
-    $('.carousel-showmanymoveone .item').each(function(){
-    var itemToClone = $(this);
-    
-    for (var i=1;i<6;i++) {
-    itemToClone = itemToClone.next();
-    
-    if (!itemToClone.length) {
-    itemToClone = $(this).siblings(':first');
-    }
-    
-    itemToClone.children(':first-child').clone()
-    .addClass("cloneditem-"+(i))
-    .appendTo($(this));
-    }
-    });
-    });
-    
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
