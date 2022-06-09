b578bbee4c5f3a3a5c2dfd4cbdef45e7a616f8f0
Pconti10 <pconti10@gmail.com>
2022-06-09 17:38:26 -0300

    terminando web

--- JS/js.js
+++ JS/js.js
@@ -1,27 +1,20 @@
-$(document).ready(function($) {
-  $('.card-slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 2000,
-    arrows: true,
-    responsive: [{
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-    {
-       breakpoint: 400,
-       settings: {
-          arrows: false,
-          slidesToShow: 1,
-          slidesToScroll: 1
-       }
-    }]
-});
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  effect: 'fade',
+  loop: true,
+  speed: 300,
+  mousewheel: {
+    invert: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
 });
